7e5b91d853d144cc5b3b3d689b77974198c02dd4
半口绵羊 <1911966901@qq.com>
2025-11-17 21:54:26 +0800

    完成了feign配置，实现各种拦截和调用微服务

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,12 +1,26 @@
 package com.hmall.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 
 public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+    @Bean
+    public RequestInterceptor userInfoRequestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(feign.RequestTemplate template) {
+                Long userId = UserContext.getUser();
+                if (userId != null) {
+                    template.header("user-info", userId.toString());
+                }
+            }
+        };
     }
 
 }
