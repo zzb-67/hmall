5db45c9174783b8640bb98a4bbcbc9b2148319e7
半口绵羊 <1911966901@qq.com>
2025-11-17 10:26:18 +0800

    Initial commit: Spring Cloud Gateway project setup

---
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -0,0 +1,25 @@
+package com.hmall.api.client;
+
+
+import com.hmall.api.dto.ItemDTO;
+// ✅ trade-service 正确示范
+import com.hmall.api.dto.OrderDetailDTO;  // 使用统一的DTO
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+import java.util.List;
+
+@FeignClient("item-service")
+public interface ItemClient {
+    @GetMapping("/items")
+    List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+
+    @PutMapping("/items/stock/deduct")
+    void deductStock(@RequestBody List<OrderDetailDTO> items);
+
+}
